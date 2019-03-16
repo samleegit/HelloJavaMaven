c7d33a9f8d04224afde7f1f36a2739f9aabe9fd7
SamLee <312627224@qq.com>
2019-03-17 07:22:18 +0800

    add getNum for HelloDongnao.java

--- src/main/java/com/dongnao/HelloDongnao.java
+++ src/main/java/com/dongnao/HelloDongnao.java
@@ -14,8 +14,20 @@ public class HelloDongnao {
 	public static void main(String[] args) {
 		HelloDongnao test = new HelloDongnao();
 		String helloString = test.sayHello("dongnao");
+		
 		JSONObject a = new JSONObject();
 		a.put("helloString", helloString);
+		
 		System.out.println(a.get("helloString"));
+		
+		test.getNum():
+	}
+	
+	public float getNum(){
+		
+		System.out.println("chu fa!");
+		float a = 5 / 0;
+		
+		return a;		
 	}
 }
