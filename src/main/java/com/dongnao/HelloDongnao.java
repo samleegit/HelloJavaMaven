6f14d7c3a9812d6adbf4754c6be77b9a0eeefb0e
SamLee <312627224@qq.com>
2019-03-22 22:43:58 +0800

    fixed bugs

--- src/main/java/com/dongnao/HelloDongnao.java
+++ src/main/java/com/dongnao/HelloDongnao.java
@@ -25,8 +25,7 @@ public class HelloDongnao {
 	
 	public float getNum(){
 		
-		System.out.println("chu fa!");	
-		float a = 5 / 0; 
-		return a;		
+		System.out.println("chu fa!");	 
+		return 5 / 2;		
 	}
 }
